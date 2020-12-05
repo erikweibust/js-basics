7b9cd9e5dd8f21dc311a5ccb4d5e3f9f778b7bc3
Erik Weibust <erik@weibust.net>
2020-12-04 19:09:39 -0600

    working through array lessons

--- index.js
+++ index.js
@@ -1,254 +1,81 @@
-// Objects
+// Arrays
 
-const circles = {
-   radius: 1,
-   location: {
-      x: 1,
-      y: 1
-   },
-   isVisible: true,
-   draw: function() {
-      console.log('draw');
-   }
-};
+const numbers = [3, 4];
 
-// factory functions
-//
-// need a way to create circle objects without copying the whole object
-function createCircle(radius) {
-   return {
-      // this can be simplified when key and value match
-      //radius: radius,
-      radius, 
-      draw() {
-         console.log('draw');
-      }
-   };
-}
+// End
+numbers.push(5, 6);
+console.log(numbers);
 
-const circle1 = createCircle(1);
-console.log(circle1);
-circle1.draw();
+// Beginning
+numbers.unshift(1, 2);
+console.log(numbers);
 
-// Constructor Functions
-function Circle(radius) {
-   this.radius = radius;
-   this.draw = function() {
-      console.log('draw');
-   }
-}
+// Middle
+numbers.splice(2, 0, 'a', 'b');
+console.log(numbers);
 
-const ccircle = new Circle(1);
-console.log( ccircle === circle1);
-console.log( ccircle == circle1);
+// Finding elements (primitives)
+const numbers1 = [1, 2, 3, 1, 4];
+console.log(numbers1.indexOf('1'));
+console.log(numbers1.indexOf(1));
+console.log(numbers1.lastIndexOf(1));
 
-// Value vs Reference Types
-//
-// Primitives are copied by their value
-// Objects are copied by their reference
-let number = 10;
+console.log(numbers1.indexOf(1) !== -1);
+console.log(numbers1.includes(1)); // new call in JS arrays
 
-function increase(number) {
-   number++;
-   console.log('in increase ' + number);
-}
-increase(number);
-console.log(number);
-
-let obj = { value: 10 };
-function increase2(obj) {
-   obj.value++;
-   console.log('in increase2 ' + obj.value);
-}
-increase2(obj);
-console.log(obj.value);
-
-// Enumerating Properies of an Object
-//
-// for-in for-of loops
-
-const circle = {
-   radius: 1,
-   draw() {
-      console.log('draw');
-   }
-};
-circle.draw();
-
-// for-in
-// to get the value of a property you use the bracket notation
-for (let key in circle) {
-   console.log(key);
-   console.log(key, circle[key]);
-}
-
-// for-of
-// works on an iteratable; not an object
-// but we can make it work....
-for (let key of Object.keys(circle)) {
-   console.log(key);
-}
-/*
-   can use for-of w/ Object.entries or Object.keys
-*/ 
-for (let key of Object.keys(circle) )
-   console.log(key); // keys as String[]
-
-for (let entry of Object.entries(circle) )
-   console.log(entry); // each key=value pair as array
-
-// if you want to know if an object has a property
-if ('radius' in circle) console.log('yes');
-if ('draw' in circle) console.log('yes');
-
-// Cloning an Object
-// 3 different methods
-const anotherCircle = {};
-for (let key in circle)
-   anotherCircle[key] = circle[key];
-
-const anotherCircle2 = Object.assign({}, circle);
-
-// spread operator
-const anotherCircle3 = { ...circle };
-
-console.log( anotherCircle );
-
-// Math examples
-function getRandomArbitrary(min, max) {
-   return Math.random() * (max - min) + min;
- }
- console.log(getRandomArbitrary(1, 11));
-
- // String examples
- // string primitive
-let message = 'hi'; //typeof 'string'
-
- // string object
- const another = new String('hi'); // using string constructor we get a typeof 'object'
-
-message = 'This is my first message';
-message.split(' ');
-
-// String template literals
-const name = 'Erik';
-const greeting = `Hi ${name},`
-
-// Date
-const now = new Date(); // new date with the constructor function.
-const date1 = new Date('May 11 2018');
-const date2 = new Date(2018, 4, 11, 9);
-console.log(now);
-console.log(date1);
-console.log(date2);
-
-// Exercise 1 - Address Object
-const address = { // object literal
-   street: '2341 Boxwood Dr',
-   city: 'Allen',
-   zipCode: 75013
-};
-
-function showAddress(address){
-   return `${address.street}
-${address.city} ${address.zipCode}`
-}
-
-function showAddress2(address) {
-   for(let key in address)
-      console.log(key, address[key]);
-}
-
-console.log(showAddress(address));
-showAddress2(address);
-
-// Exercise 2 - Factory and Constructor Functions
-
-// Factory Function
-function createAddress(street, city, zipCode) {
-   return {
-      street:street,
-      city:city,
-      zipCode // if key and value are same you can drop value
-   };
-}
-const add1 = createAddress('2341 Boxwood', 'Allen', 75013);
-console.log(add1);
-
-// Constructor Function
-function Address(street, city, zipCode) {
-   this.street = street;
-   this.city = city;
-   this.zipCode = zipCode;
-}
-
-const add2 = new Address('2341 Boxwood', 'Allen', 75013);
-const add3 = new Address('2341 Boxwood', 'Allen', 75013);
-const add4 = new Address('2341 Boxwood', 'Allen', 75012);
-const add5 = add4;
-console.log(add2);
-console.log(add3);
-
-// Exercise 3 - Object Equality
-
-function areEqual(address1, address2) {
-   return address1.street === address2.street &&
-         address1.city === address2.city &&
-         address1.zipCode === address2.zipCode;
-}
-
-console.log(`should be true ${areEqual(add2, add3)}`);
-console.log(`should be false ${areEqual(add3, add4)}`);
-
-function areSame(address1, address2) {
-   return address1 === address2;
-}
-console.log('should be false ' + areSame(add2, add3));
-console.log('should be true ' + areSame(add4, add5));
-
-// Exercise 4 - Blog Post object
-
-let blogPost = {
-   title: 'Hello World',
-   body: 'This is my Hello World blog post.',
-   author: 'Erik Weibust',
-   views: 100000,
-   comments: [
-      { 
-         author: 'Katie Weibust',
-         body: 'My husband is so smart'
-      },
-      {
-         author: 'Katie Weibust',
-         body: 'And... He\'s so cute.'
-      }
-   ],
-   isLive: false
-};
-
-console.log(blogPost);
-
-// Exercise 5 - Blog Post Constructor Function
-function BlogPost(title, body, author) {
-   this.title = title;
-   this.body = body;
-   this.author = author;
-   this.views = 0;
-   this.comments = [];
-   this.isLive = false;
-}
-
-let post = new BlogPost('Hello', 'Gonna be a great post', 'Erik Weibust');
-console.log(post);
-
-// Exercise 6 - Price Range Object
-
-let priceRanges = [
-   { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
-   { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
-   { label: '$$$', tooltip: 'Expensive', minPerPerson: 20, maxPerPerson: 50 }
+// Finding elements (Reference Types)
+const courses = [
+   { id: 1, name: 'Intro to Math' },
+   { id: 2, name: 'English 101' }
 ];
 
-let restaurants = [
-   { averagePerPerson: 5 }
+const course1 = courses.find(function(course) {
+   return course.name === 'English 101';
+});
+console.log(course1);
+
+const course2 = courses.find(function(course) {
+   return course.name === 'English';
+});
+console.log(course2);
+
+const course3 = courses.find(function(course) {
+   return course.name === 'Intro to Math';
+});
+console.log(course3);
+
+// Arrow Functions
+const coursess = [
+   { id: 1, name: 'Physics' },
+   { id: 2, name: 'English 101' }
 ];
+// arrow functions improve on the find/function by simplifying the syntax
+const course4 = coursess.find(function(course) {
+   return course.name === 'Intro to Math';
+});
+console.log(course4);
+/* step by step
+1. remove the keyword function and use => to seperate the parameter from function body 
+*/
+const course5 = coursess.find((course) => {
+   return course.name === 'English 101';
+});
+console.log(course5);
+
+// we can do better... with a single parameter we can remove the parentheses
+const course6 = courses.find( course => {
+   return course.name === 'English 101';
+});
+console.log(course6);
+// if there were no params, you would use empty ()
+const course7 = courses.find( () => {
+   //return course.name === 'English 101';
+});
+console.log(course7);
+
+// finally, if it's a single line of code returning a value,
+// you can remove the return keyword and drop the {}
+// you read this as: course goes to (expression) or...
+// you are finding a course with the id of 1
+const course8 = coursess.find(course => course.id === 1);
+console.log(course8);
---
+++ index3.js
@@ -0,0 +1,254 @@
+// Objects
+
+const circles = {
+   radius: 1,
+   location: {
+      x: 1,
+      y: 1
+   },
+   isVisible: true,
+   draw: function() {
+      console.log('draw');
+   }
+};
+
+// factory functions
+//
+// need a way to create circle objects without copying the whole object
+function createCircle(radius) {
+   return {
+      // this can be simplified when key and value match
+      //radius: radius,
+      radius, 
+      draw() {
+         console.log('draw');
+      }
+   };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+circle1.draw();
+
+// Constructor Functions
+function Circle(radius) {
+   this.radius = radius;
+   this.draw = function() {
+      console.log('draw');
+   }
+}
+
+const ccircle = new Circle(1);
+console.log( ccircle === circle1);
+console.log( ccircle == circle1);
+
+// Value vs Reference Types
+//
+// Primitives are copied by their value
+// Objects are copied by their reference
+let number = 10;
+
+function increase(number) {
+   number++;
+   console.log('in increase ' + number);
+}
+increase(number);
+console.log(number);
+
+let obj = { value: 10 };
+function increase2(obj) {
+   obj.value++;
+   console.log('in increase2 ' + obj.value);
+}
+increase2(obj);
+console.log(obj.value);
+
+// Enumerating Properies of an Object
+//
+// for-in for-of loops
+
+const circle = {
+   radius: 1,
+   draw() {
+      console.log('draw');
+   }
+};
+circle.draw();
+
+// for-in
+// to get the value of a property you use the bracket notation
+for (let key in circle) {
+   console.log(key);
+   console.log(key, circle[key]);
+}
+
+// for-of
+// works on an iteratable; not an object
+// but we can make it work....
+for (let key of Object.keys(circle)) {
+   console.log(key);
+}
+/*
+   can use for-of w/ Object.entries or Object.keys
+*/ 
+for (let key of Object.keys(circle) )
+   console.log(key); // keys as String[]
+
+for (let entry of Object.entries(circle) )
+   console.log(entry); // each key=value pair as array
+
+// if you want to know if an object has a property
+if ('radius' in circle) console.log('yes');
+if ('draw' in circle) console.log('yes');
+
+// Cloning an Object
+// 3 different methods
+const anotherCircle = {};
+for (let key in circle)
+   anotherCircle[key] = circle[key];
+
+const anotherCircle2 = Object.assign({}, circle);
+
+// spread operator
+const anotherCircle3 = { ...circle };
+
+console.log( anotherCircle );
+
+// Math examples
+function getRandomArbitrary(min, max) {
+   return Math.random() * (max - min) + min;
+ }
+ console.log(getRandomArbitrary(1, 11));
+
+ // String examples
+ // string primitive
+let message = 'hi'; //typeof 'string'
+
+ // string object
+ const another = new String('hi'); // using string constructor we get a typeof 'object'
+
+message = 'This is my first message';
+message.split(' ');
+
+// String template literals
+const name = 'Erik';
+const greeting = `Hi ${name},`
+
+// Date
+const now = new Date(); // new date with the constructor function.
+const date1 = new Date('May 11 2018');
+const date2 = new Date(2018, 4, 11, 9);
+console.log(now);
+console.log(date1);
+console.log(date2);
+
+// Exercise 1 - Address Object
+const address = { // object literal
+   street: '2341 Boxwood Dr',
+   city: 'Allen',
+   zipCode: 75013
+};
+
+function showAddress(address){
+   return `${address.street}
+${address.city} ${address.zipCode}`
+}
+
+function showAddress2(address) {
+   for(let key in address)
+      console.log(key, address[key]);
+}
+
+console.log(showAddress(address));
+showAddress2(address);
+
+// Exercise 2 - Factory and Constructor Functions
+
+// Factory Function
+function createAddress(street, city, zipCode) {
+   return {
+      street:street,
+      city:city,
+      zipCode // if key and value are same you can drop value
+   };
+}
+const add1 = createAddress('2341 Boxwood', 'Allen', 75013);
+console.log(add1);
+
+// Constructor Function
+function Address(street, city, zipCode) {
+   this.street = street;
+   this.city = city;
+   this.zipCode = zipCode;
+}
+
+const add2 = new Address('2341 Boxwood', 'Allen', 75013);
+const add3 = new Address('2341 Boxwood', 'Allen', 75013);
+const add4 = new Address('2341 Boxwood', 'Allen', 75012);
+const add5 = add4;
+console.log(add2);
+console.log(add3);
+
+// Exercise 3 - Object Equality
+
+function areEqual(address1, address2) {
+   return address1.street === address2.street &&
+         address1.city === address2.city &&
+         address1.zipCode === address2.zipCode;
+}
+
+console.log(`should be true ${areEqual(add2, add3)}`);
+console.log(`should be false ${areEqual(add3, add4)}`);
+
+function areSame(address1, address2) {
+   return address1 === address2;
+}
+console.log('should be false ' + areSame(add2, add3));
+console.log('should be true ' + areSame(add4, add5));
+
+// Exercise 4 - Blog Post object
+
+let blogPost = {
+   title: 'Hello World',
+   body: 'This is my Hello World blog post.',
+   author: 'Erik Weibust',
+   views: 100000,
+   comments: [
+      { 
+         author: 'Katie Weibust',
+         body: 'My husband is so smart'
+      },
+      {
+         author: 'Katie Weibust',
+         body: 'And... He\'s so cute.'
+      }
+   ],
+   isLive: false
+};
+
+console.log(blogPost);
+
+// Exercise 5 - Blog Post Constructor Function
+function BlogPost(title, body, author) {
+   this.title = title;
+   this.body = body;
+   this.author = author;
+   this.views = 0;
+   this.comments = [];
+   this.isLive = false;
+}
+
+let post = new BlogPost('Hello', 'Gonna be a great post', 'Erik Weibust');
+console.log(post);
+
+// Exercise 6 - Price Range Object
+
+let priceRanges = [
+   { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+   { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+   { label: '$$$', tooltip: 'Expensive', minPerPerson: 20, maxPerPerson: 50 }
+];
+
+let restaurants = [
+   { averagePerPerson: 5 }
+];
